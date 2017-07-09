4182aabc6409f80ccc4082b04660989e48d8dcaa
Kristina Born <kristinab@nulogy.com>
2017-07-09 15:56:40 -0400

    update path

--- spec/requests/api/campaigns_create_spec.rb
+++ spec/requests/api/campaigns_create_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe '/api/campaigns', type: :request do
   describe "POST#create" do
     it 'creates a new campaign' do
       expect do
-        post campaigns_path, params: { campaign: { name: "Solo Campaign", cycle: "The Dunwich Legacy" } }
+        post api_campaigns_path, params: { campaign: { name: "Solo Campaign", cycle: "The Dunwich Legacy" } }
       end.to change { Campaign.count }.by(1)
 
-      expect(response).to redirect_to edit_campaign_url(Campaign.last)
+      expect(response).to have_http_status(200)
     end
   end
 end
