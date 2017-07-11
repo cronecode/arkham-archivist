63f58bb3759d2f12d0e9a13c381cf2489a3bfd50
Kristina Born <kristinab@nulogy.com>
2017-07-11 08:27:16 -0400

    test coverage for generating scenarios during campaign creation

--- spec/requests/api/campaigns_create_spec.rb
+++ spec/requests/api/campaigns_create_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe '/api/campaigns', type: :request do
   describe "POST#create" do
     it 'creates a new campaign' do
       expect do
-        post api_campaigns_path, params: { campaign: { name: "Solo Campaign", cycle: "The Dunwich Legacy" } }
+        post api_campaigns_path, params: { campaign: { name: "Solo Campaign", cycle: "The Dunwich Legacy" }}
       end.to change { Campaign.count }.by(1)
 
       id = Campaign.last.id
@@ -16,6 +16,19 @@ RSpec.describe '/api/campaigns', type: :request do
           id: id
         }
       )
+    end
+
+    it 'campaign scenarios are generated during creation according to campaign cycle' do
+      post api_campaigns_path, params: { campaign: { name: "2-Player Campaign", cycle: "Night of the Zealot" }}
+
+      scenarios = Campaign.last.scenarios.order(:order)
+      first = "The Gathering"
+      second = "Midnight Masks"
+      third = "The Devourer Below"
+
+      expect(scenarios[0].name).to eq(first)
+      expect(scenarios[1].name).to eq(second)
+      expect(scenarios[2].name).to eq(third)
     end
   end
 end
