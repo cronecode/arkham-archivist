4ea870b0a164e079d2133192696b810b624a8f82
Kristina Born <kristinab@nulogy.com>
2017-07-11 08:29:44 -0400

    automatically adds the cycle's scenarios to a newly created campaign

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -6,11 +6,13 @@ module Api
     end
 
     def create
-      campaign = Campaign.new(campaign_params)
-      if campaign.save
+      @campaign = Campaign.new(campaign_params)
+
+      if @campaign.save
+        @campaign.scenarios << cycle_scenarios
         render json: {
           data: {
-            id: campaign.id
+            id: @campaign.id,
           }, status: 200
         }
       end
@@ -20,6 +22,11 @@ module Api
 
     def campaign_params
       params.require(:campaign).permit(:cycle, :id, :name)
+    end
+
+    def cycle_scenarios
+      scenario_names = Scenario::CYCLE_SCENARIOS_MAPPING[@campaign.cycle]
+      scenario_names.map { |name| Scenario.new(campaign: @campaign, name: name, order: scenario_names.find_index(name) + 1)}
     end
   end
 end
