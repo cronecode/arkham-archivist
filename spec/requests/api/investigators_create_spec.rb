03db5be9b48ec5ddddfc9de3ead697801ee946ad
Kristina Born <kristinab@nulogy.com>
2017-07-12 07:24:28 -0400

    test coverage for investigator creation

---
+++ spec/requests/api/investigators_create_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "/api/campaigns/:campaign_id/investigators", type: :request do
+  describe "POST#create" do
+    it "creates a new investigator" do
+      campaign = FactoryGirl.create(:campaign)
+
+      expect do
+        post api_campaign_investigators_path(campaign), params: { investigator: { name: "Jim Culver" }}
+      end.to change { Investigator.count }.by(1)
+
+      id = Investigator.last.id
+
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(
+        data: {
+          id: id
+        }
+      )
+    end
+  end
+end
